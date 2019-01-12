55543e3e9c9fa0297acb48ca0b45785c1c83adde
Mark Bennett <mark.bennett@ucf.edu>
2019-01-11 21:03:29 -0500

    initial commit

---
+++ provost.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Provost Addon
+Plugin URI:  
+Description: Adds functinality to the provost website
+Version:     1.00 beta
+Author:      Mark Bennett
+Author URI:  
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+if ( ! defined( ‘ABSPATH’ ) ) exit; // Exit if accessed directly
+
+
+define('CD_SLL_PATH', plugin_dir_path(__FILE__));
+define('CD_SLL_NAME', plugin_basename(__FILE__));
+
+
+/*
+* Templates
+*/
+
+//Initiatives template
+
